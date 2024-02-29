d07d8d4d39e20b6f57fbfce262f24ee32200a399
Harer <100375276+napkngayngo@users.noreply.github.com>
2024-02-29 16:22:59 +0700

    Create Sidebar, Navigation and Layout

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,89 +1,97 @@
 "use client";
 
 import axios from "axios";
-import { useCallback, useState } from "react";
-import {
-  FieldValue,
-  FieldValues,
-  SubmitHandler,
-  useForm,
-} from "react-hook-form";
+import { signIn, useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import Input from "../../components/input/Input";
-import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
-import { BsGithub, BsGoogle } from "react-icons/bs";
+import Button from "@/app/components/Button";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/users');
+    }
+  }, [session?.status, router]);
+
   const toggleVariant = useCallback(() => {
-    if (variant === "LOGIN") {
-      setVariant("REGISTER");
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
     } else {
-      setVariant("LOGIN");
+      setVariant('LOGIN');
     }
   }, [variant]);
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { 
+      errors,
+    }
   } = useForm<FieldValues>({
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
+      name: '',
+      email: '',
+      password: ''
+    }
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    if (variant === "REGISTER") {
+    if (variant === 'REGISTER') {
       axios.post('/api/register', data)
-      .catch(() => toast.error('Something went wrong!'))
-      .finally(() => setIsLoading(false))
+      .then(() => signIn('credentials', data))
+      .catch(() => toast.error("Something went wrong!"))
+      .finally(() => setIsLoading(false));
     }
 
-    if (variant === "LOGIN") {
+    if (variant === 'LOGIN') {
       signIn('credentials', {
         ...data,
         redirect: false
       })
-      .then((callback) => {
-        if (callback?.error) {
-          toast.error('Invalid credentials');
-        }
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error('Invalid credentials!');
+          }
 
-        if (callback?.ok && !callback?.error) {
-          toast.success('Logged in!')
-        }
-      })
-      .finally(() => setIsLoading(false));
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+            router.push("/users");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
-  }
-  
+  };
+
   const socialAction = (action: string) => {
     setIsLoading(true);
 
     signIn(action, { redirect: false })
-    .then((callback) => {
-      if (callback?.error) {
-        toast.error('Invalid Credentials');
-      }
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials");
+        }
 
-      if (callback?.ok && !callback?.error) {
-        toast.success('Logged in!')
-      }
-    })
-    .finally(() => setIsLoading(false));
-  }
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <div
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
 import "./globals.css";
 
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
